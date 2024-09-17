6b5ca7fd280782337c390a09ea77cd73c169b64c
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2024-09-16 21:59:46 -0300

    add few tweaks to console.log

--- decimalToBase.js
+++ decimalToBase.js
@@ -1,5 +1,4 @@
 function decimalToBase(decimal = 0, base = 2) {
-  console.log({ decimal, base })
   if (base < 2 || base > 64) throw Error(`Base cannot be lesser than 2 or greater than 64`)
   if (decimal == 0) return "0";
 
@@ -29,7 +28,6 @@ function decimalToBase(decimal = 0, base = 2) {
   while (remainderStack.length) {
     if (isBinary && remainderStack.length % 4 == 0 && remainderStack.length !== newRemainderStackInitialLength) binaryValue += " ";
     const innerValue = remainderStack.pop()
-    console.log({ innerValue, base })
     binaryValue += digits[innerValue]
   }
 
@@ -39,19 +37,19 @@ function decimalToBase(decimal = 0, base = 2) {
 
 
 console.log({
-  resultFor1: decimalToBase(1),
-  resultFor2: decimalToBase(2),
-  resultFor3: decimalToBase(3),
-  resultFor10: decimalToBase(10),
-  resultFor12: decimalToBase(12),
-  resultFor18: decimalToBase(18),
-  resultFor64: decimalToBase(64),
-  resultFor128: decimalToBase(128),
-  resultFor255: decimalToBase(255, 16),
-  resultFor256: decimalToBase(256, 16),
-  resultFor16: decimalToBase(15, 16),
-  resultFor4096: decimalToBase(4096),
-  resultFor39: decimalToBase(39),
-  resultFor63Base64: decimalToBase(63, 64),
-  resultFor64Base64: decimalToBase(64, 64)
+  resultBase2For1: decimalToBase(1),
+  resultBase2For2: decimalToBase(2),
+  resultBase2For3: decimalToBase(3),
+  resultBase2For10: decimalToBase(10),
+  resultBase2For12: decimalToBase(12),
+  resultBase2For18: decimalToBase(18),
+  resultBase2For39: decimalToBase(39),
+  resultBase2For64: decimalToBase(64),
+  resultBase2For128: decimalToBase(128),
+  resultBase2For4096: decimalToBase(4096),
+  resultBase16For255: decimalToBase(255, 16),
+  resultBase16For256: decimalToBase(256, 16),
+  resultBase16For16: decimalToBase(15, 16),
+  resultBase64For63: decimalToBase(63, 64),
+  resultBase64For64: decimalToBase(64, 64)
 })
